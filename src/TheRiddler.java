05927a37da3aa7890fc6c281c9f4102190282ce7
DavidAndBlake <League@administorsimac.attlocal.net>
2017-12-09 12:58:07 -0800

    finished the geeks and riddle and another one

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -8,17 +10,42 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score= 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+String riddle=JOptionPane.showInputDialog("I stay with you all day, but not night. What am I?");
+		String ans= "your shadow";
+// 4. If they got the answer right, pop up "correct!" and increase the score by one
+if(riddle.equals(ans)) {
+	JOptionPane.showMessageDialog(null, "Correct");
+	score++;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "WRONG, the real answer is your shadow");
+}
 		// 6. Add some more riddles
 
-		// 2. Make a pop up to show the score.
-		
+riddle=JOptionPane.showInputDialog("I gurgle but never speak, run but never walk, have a bed but never sleeps. What am I?");
+ ans= "a river";
+//4. If they got the answer right, pop up "correct!" and increase the score by one
+if(riddle.equals(ans)) {
+JOptionPane.showMessageDialog(null, "Correct");
+score++;
+}
+// 5. Otherwise, say "wrong" and tell them the answer
+else {
+JOptionPane.showMessageDialog(null, "WRONG, the correct answer is a river");
+}
+
+
+
+
+
+
+
+
+// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "score: " + score);
 	}
 }
 
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,22 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
+String matt= "talking to cats";
+String jaren= "teleportation";
+String kevin = "invisibility";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+String name = JOptionPane.showInputDialog("enter your name");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
-	}
+if(name.equals("matt")) {
+	 JOptionPane.showMessageDialog(null,"your superpower is " + matt);
+}
+	 if(name.equals("jaren")) {
+	 JOptionPane.showMessageDialog(null, "your superpower is " + jaren);
+	  }
+	 if(name.equals("kevin")) {
+		 JOptionPane.showMessageDialog(null, "your superpower is " + kevin);
+	 }
+}
 }
 
